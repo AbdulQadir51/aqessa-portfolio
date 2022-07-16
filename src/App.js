10991fd5116430d10fbb8452fbc8e2aae237bb48
TahirMurtaza <abdulessa93@gmail.com>
2022-07-16 21:18:06 +0500

    Footer component added to app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-import About from './components/About';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 
 function App() {
     return (
       <div>
         <Header></Header>
-      {}
+        {}
+      <Footer></Footer>
     </div>
        );
     }
